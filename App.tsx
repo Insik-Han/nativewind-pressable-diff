cacac41c3e0c5ba8a238c4e70e7d03a7b1037848
Insik-Han <insik.han@gaudiy.com>
2025-11-25 11:21:56 +0900

    feat: replace generic screen content with multiple `Pressable` examples demonstrating various styling and state handling methods.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,84 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { Pressable, Text, useWindowDimensions, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { useState } from 'react';
+
+const PressableWithPressedState = () => {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <Pressable
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      style={{
+        backgroundColor: pressed ? 'green' : undefined,
+      }}
+    >
+      {({ pressed }) => (
+        <Text>{pressed ? 'Pressed!' : 'PressableWithPressedState'}</Text>
+      )}
+    </Pressable>
+  );
+};
+
+const PressableWithNativewindPesudoClass = () => {
+  return (
+    <Pressable className="active:bg-green-700">
+      {({ pressed }) => (
+        <Text>
+          {pressed ? 'Pressed!' : 'PressableWithNativewindPesudoClass'}
+        </Text>
+      )}
+    </Pressable>
+  );
+};
+
+const PressableWithCssInteropFalse = () => {
+  return (
+    <Pressable
+      cssInterop={false}
+      className="bg-orange-400"
+      style={({ pressed }) => [
+        pressed
+          ? {
+            backgroundColor: 'green',
+          }
+          : {},
+      ]}
+    >
+      {({ pressed }) => (
+        <Text>
+          {pressed ? 'Pressed!' : 'PressableWithCssInteropFalse'}
+        </Text>
+      )}
+    </Pressable>
+  );
+};
 
 export default function App() {
+  const { width: windowWidth } = useWindowDimensions();
+
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <View className="justify-center flex-1">
+        <View className="flex-row flex-wrap p-4 gap-4">
+          {[
+            PressableWithPressedState,
+            PressableWithNativewindPesudoClass,
+            PressableWithCssInteropFalse,
+          ].map((Component, index) => (
+            <View
+              className="rounded-md p-2"
+              style={{
+                width: (windowWidth - 32 - 24) / 3,
+              }}
+              key={index}
+            >
+              <Component />
+            </View>
+          ))}
+        </View>
+      </View>
       <StatusBar style="auto" />
     </>
   );
